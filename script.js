f5163eb8d6a1bbcb772e1492396df8cb2de7c168
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2025-12-16 18:46:03 -0500

    Added pseudo outline + css sheet

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+PSEUDO CODE
+create 16x16 div workspace
+- flexbox to organize the grid
+- css reset borders and margins
+- change div background color on hover
+
+add prompt for user to input # of squares
+- should replace the 16x16 square
+- should respect 960px max width
+- limit input to 100 max
+
+extra credit
+- randomize square color rgb value
+- incrementally darken square by 10% on each interaction
+
+*/
